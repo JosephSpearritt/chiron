63e3f49da82076f45b6abf4767a4ce5447f7fa52
Joseph Spearritt <joe.spearritt@live.com>
2016-04-23 09:25:40 +1000

    Add dummy index page

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return redirect(url_for('dashboard'))
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run()
