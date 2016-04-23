cd234c2941fea36c7b409c4a8b300143e1a011e1
Joseph Spearritt <joe.spearritt@live.com>
2016-04-23 10:41:08 +1000

    Add Login logic (wip)

--- chiron/app.py
+++ chiron/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 # Configuration
 SECRET_KEY = 'adatabasekey'
@@ -7,5 +8,9 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///chiron.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login';
+login_manager.init_app(app)
 
 db = SQLAlchemy(app)
--- chiron/views.py
+++ chiron/views.py
@@ -6,9 +6,11 @@
 
 """
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
+from flask.ext.login import login_required, login_user, logout_user, current_user
 
 from . import app
+from .app import login_manager
 from .models import *
 
 
@@ -18,3 +20,34 @@ def index():
     Base route for the website.
     """
     return render_template('dashboard.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        sqlUser = User.query.filter_by(username=request.form.get('username', ''))
+        if sqlUser.count() == 1:
+            if sqlUser.first().check_password(request.form.get('password', '')):
+                login_user(sqlUser.first())
+                flash('Successfully logged in as %s' % request.form.get('username', ''))
+                try:
+                    next = request.form['next']
+                    return redirect(next)
+                except:
+                    return redirect(url_for('index'))
+            else:
+                flash('Login failed!')
+                return redirect(url_for('login'))
+        else:
+            flash('Login failed!')
+            return redirect(url_for('login'))
+        return
+
+    # Render the login page
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
