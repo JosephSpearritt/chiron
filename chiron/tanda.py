06620c19e4798aa2baf3f32e90ff2b3cc30b6dbc
Samuel Brown <windyce@gmail.com>
2016-04-23 10:28:11 +1000

    Added code for OAuth API, and framework deciphering texts and accessing tanda information

---
+++ chiron/tanda.py
@@ -0,0 +1,31 @@
+from .oauth import authenticate
+
+
+def get_token():
+    return authenticate("windyce@gmail.com", "tandahack2016", "user me")
+
+
+def find_employee(id,no,name,zip):
+
+    return
+
+
+def find_employee_id():
+
+    return
+
+
+def find_employee_name():
+
+    return
+
+
+def find_employee_no():
+
+    return
+
+
+def find_employee_zip():
+
+    return
+
